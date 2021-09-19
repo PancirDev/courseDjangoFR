5fb13cb68b1199d0508546d17031248a11526e00
PancirD <pancir.mail@gmail.com>
2021-09-20 00:20:22 +0300

    123

---
+++ travel/vievs.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+def home(request):
+    name = 'Kler'
+    return render(request, 'home.html', {'name': name})
+
+def about(request):
+    name = 'About text'
+    return render(request, 'about.html', {'name': name})
